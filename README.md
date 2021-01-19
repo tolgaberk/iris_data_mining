c9206de808dff27acb4c41f115fae41499fda8ba
Tolga Berk Ordanuç <tolgaberkordanuc@gmail.com>
2021-01-20 00:25:13 +0300

    added README

---
+++ README.md
@@ -0,0 +1,8 @@
+# FINAL PROJECT FOR DATA MINING CLASS
+
+## Used Technologies
+
+- Typescript -> superset of javascript with type annotations
+- Webpack -> required to compile typescript to javascript in order to run in the browser environment
+- Tensorflow JS -> google's graphics accelerated machine learning library for JavaScript
+- Plotly JS -> Python's plotly but in javascript
--- src/data/iris/csvToJson.js
+++ src/data/iris/csvToJson.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
+// a script to convert csv file to a json object
+// so i can import and work with it
 const fs = require("fs");
 
 const csv = fs.readFileSync(process.argv[2], "utf-8");
 
 const rows = csv.split("\n");
 
+// eliminate last empty row
 rows[rows.length - 1].trim() === "" && rows.pop();
 
 const headers = rows[0].split(",");
 const json = [];
 
+// loop every row and generate a object/dictionary from rows.
 for (const row of rows.slice(1)) {
   const values = row.split(",");
   const obj = {};
